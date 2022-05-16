e7fac5b601eacf436f25bb74c4e6910d184e8fe7
ChoJinmok <jjmok1127@gmail.com>
2022-05-16 21:43:14 +0900

    Add. User Introduce

--- src/pages/MyBlog/components/UserIntoroduce.js
+++ src/pages/MyBlog/components/UserIntoroduce.js
@@ -8,6 +8,21 @@ export default function UserIntroduce() {
         싶은 프론트엔드 개발자 조진목입니다.
       </IntroduceDefault>
       <IntroduceH3>Contact.</IntroduceH3>
+      <IntroduceDefault>
+        Email. <IntroduceBold>jjmok1127@gmail.com</IntroduceBold>
+      </IntroduceDefault>
+      <IntroduceH3>Channel.</IntroduceH3>
+      <IntroduceDefault>
+        GitHub. <IntroduceBold>https://github.com/ChoJinmok</IntroduceBold>
+      </IntroduceDefault>
+      <IntroduceH3>Introduce.</IntroduceH3>
+      <IntroduceDefault>
+        <IntroduceBold>
+          문제를 만나면 집요하게 파고들어 해결하는 것을 좋아합니다.
+        </IntroduceBold>{' '}
+        개발 경력에 비해 비교적 빠르게 성장했다고 생각하는데 개발을 하며 부딪힌
+        문제들을 끝까지 해결해내려 했기 때문이라 생각합니다.
+      </IntroduceDefault>
     </UserIntroduceWrap>
   );
 }
@@ -36,4 +51,8 @@ const IntroduceH3 = styled(IntroduceDefault)`
   font-size: 16px;
   font-weight: 400;
   line-height: 21px;
+`;
+
+const IntroduceBold = styled.span`
+  font-weight: 400;
 `;
