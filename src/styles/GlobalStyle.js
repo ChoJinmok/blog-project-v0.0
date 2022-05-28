355d25aff7bfbce5e9a905c45a4798b14bc05b8a
ChoJinmok <jjmok1127@gmail.com>
2022-05-28 22:35:43 +0900

    Hotfix. 폰트 적용

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,21 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import NotoSansRegular from './fonts/Noto_Sans/NotoSans-Regular.ttf';
-import NotoSansKRRegular from './fonts/Noto_Sans_KR/NotoSansKR-Regular.otf';
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-      font-family: "Noto Sans Regular";
-      src: local("Noto Sans Regular"),
-      url(${NotoSansRegular}) format('ttf');
-  }
-
-  @font-face {
-      font-family: "Noto Sans KR Regular";
-      src: local("Noto Sans KR Regular"),
-      url(${NotoSansKRRegular}) format('otf');
-  }
-  
   ${reset}
 	
 	* {
@@ -30,7 +16,7 @@ const GlobalStyle = createGlobalStyle`
   html,
   body {
     height: 100%;
-    font-family: 'Noto Sans KR Regular', 'Noto Sans Regular';
+    font-family: 'Noto Sans KR';
   }
 	
 `;
